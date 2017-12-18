b69c205995460c93c642d3072404c2faf167c9d4
nige-personal <nigelsurtees@hotmail.co.uk>
2017-12-18 23:25:10 +0000

    add download report

--- lib/s3_report_helper.rb
+++ lib/s3_report_helper.rb
@@ -8,7 +8,7 @@ class S3ReportHelper
     @tmp_filepath = local_temp_dir
   end
 
-  def get_report(year, business_npwd, report_type)
+  def get_report(year, business_npwd, report_type, ext = DEFAULT_FILE_EXT)
     s3 = Aws::S3::Client.new
     s3_filename = build_filename(year, business_npwd, report_type, ext)
     resp = s3.get_object({ bucket: report_bucket_name, key: s3_filename, target: target })
